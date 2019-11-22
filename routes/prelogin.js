3cafef942fce5c6c05345ebd59e929fa862934e6
Griffith <2ndgriff@gmail.com>
2019-11-22 17:13:58 +0100

    copied auth route from code along, currently not functioning

--- routes/prelogin.js
+++ routes/prelogin.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-//const signupRouter = require('./signup');
+const signupRouter = require('./signup');
 
-//router.use('/signup',signupRouter)
+router.use('/signup',signupRouter)
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', {title: 'Login to Petsit'});
+  res.render('prelogin-views/login', {title: 'Login to Petsit'});
 });
 
 module.exports = router;
