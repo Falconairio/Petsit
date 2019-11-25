81b61aae362417a1dba553b5e891331e125d60bf
Griffith <2ndgriff@gmail.com>
2019-11-25 15:37:52 +0100

    added working button redirects and made a profile route

--- routes/postlogin.js
+++ routes/postlogin.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const logoutRouter = require('./logout');
+const profileRouter = require('./profile')
 
 router.use('/logout', logoutRouter);
+router.use('/profile', profileRouter);
 
 router.get('/home', function(req, res, next) {
   res.render('postlogin-views/homepage', {title: 'Welcome to Petsit'});
