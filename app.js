cc8382f8560017bb74193fe2154d29a84551dbc1
Fred011 <54903136+Fred011@users.noreply.github.com>
2019-11-26 23:31:34 +0100

    partial request created and request side scroll menu done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const hbs = require('hbs');
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(
   session({
