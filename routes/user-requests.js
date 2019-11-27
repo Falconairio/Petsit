cc9aa8da3b2a69757717f1b28b2ff237b82727cb
Griffith <2ndgriff@gmail.com>
2019-11-27 11:10:42 +0100

    merge fix commit

--- routes/user-requests.js
+++ routes/user-requests.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
   router.get('/', (req, res, next) => {
   User.findById(req.session.currentUser._id)
     .then( (user) => {
-      if(user.pets === null) {
+      if(!user.pets) {
         console.log(user);
           res.render('postlogin-views/add-pet', {title: 'Add a Pet'});
       } else {
