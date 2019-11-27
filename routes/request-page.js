2a808efded1b441fec5610992fdb8cc198af98cc
Fred011 <54903136+Fred011@users.noreply.github.com>
2019-11-27 18:45:35 +0100

    style

--- routes/request-page.js
+++ routes/request-page.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const Pet = require('./../models/Pet');
+const Request = require('./../models/Request')
+
+
+
+router.get('/:type', (req, res, next) => {
+  console.log('heeeeeeeeeere',req.params);
+  Request.find({}).populate('pet')
+  .then( (data) => console.log(data))
+  .catch( (err) => console.log(err));
+})
+
 
 router.get('/', (req, res, next) => {
-  res.render('postlogin-views/request-page', {title: 'Request'});
+  Request.findById(req.session.currentUser._id)
+    .then((result) => {
+      Pet.findById(result.pets)
+      .then( (data) => {
+        res.render('postlogin-views/request-page', {title: 'Request', request: result, pet: data});
+      })
+      .catch( (err) => console.log(err));
+    }).catch((err) => {
+      console.log(err);
+    })
+  
 });
 
+
+
 module.exports = router;
+ 
