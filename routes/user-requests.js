30f7f914f223bd50629b3d8f98ea7da080e9d2c1
Griffith <2ndgriff@gmail.com>
2019-11-27 12:07:08 +0100

    added name display in request menu

--- routes/user-requests.js
+++ routes/user-requests.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const User = require('./../models/User');
+const Pet = require('./../models/Pet');
 
   router.get('/', (req, res, next) => {
   User.findById(req.session.currentUser._id)
@@ -9,8 +10,11 @@ const User = require('./../models/User');
         console.log(user);
           res.render('postlogin-views/add-pet', {title: 'Add a Pet'});
       } else {
-          console.log('bruh');
-          res.render('postlogin-views/user-requests', {title: 'Create a request'});
+          Pet.findById(user.pets._id)
+          .then( (pet) => {
+            res.render('postlogin-views/user-requests', {title: 'Create a request' ,pet: pet});
+          })
+          .catch( (err) => console.log(err));
       }
     })  
     .catch( (err) => console.log(err)); 
