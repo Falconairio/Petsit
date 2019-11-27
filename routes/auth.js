9b45ac3222c6627ec5f90c9d17042189e7e2a876
Griffith <2ndgriff@gmail.com>
2019-11-27 16:39:18 +0100

    updated displayed information in pet page

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ var express = require('express');
 const zxcvbn = require('zxcvbn');
 const User = require('./../models/User');
 const Pet = require('./../models/Pet');
-const Request = require('./../models/Request')
+const Request = require('./../models/Request');
 var router = express.Router();
 const parser = require('./../config/cloudinary');
 
@@ -112,11 +112,11 @@ router.post('/add-pet', parser.single('picture'), (req, res, next) => {
 
 
   // 2 - Destructure the password and username
-  const { name , age, breed , description  } = req.body;
+  const { name , age, breed, petType , description  } = req.body;
 
       const image_url = req.file.secure_url // to get the image with cloudinary
       // > Create the user in the DB
-      Pet.create({ name, age, petPictureUrl: image_url, description, petType: null, requests: null, breed})
+      Pet.create({ name, age, petPictureUrl: image_url, petType, description, requests: null, breed})
         .then(newPetObj => {
           
           User.findById(req.session.currentUser._id)
--- routes/request-page.js
+++ routes/request-page.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const Pet = require('./../models/Pet');
+const Request = require('./../models/Request');
 
 router.get('/', (req, res, next) => {
-  res.render('postlogin-views/request-page', {title: 'Request'});
+  console.log(req.query);
+  Request.findById(req.query.id)
+  .then( (data) => {
+    Pet.findById(data.pet)
+    .then( (petData) => {
+      res.render('postlogin-views/request-page', {title: 'Request', request: data, pet: petData});
+    })
+    .catch( (err) => console.log(err));
+  })
+  .catch( (err) => console.log(err));
 });
 
 module.exports = router;
